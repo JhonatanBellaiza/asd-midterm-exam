39e5b794acce1f44697b5520d6fae58e3f2ddeeb
Jhonatan <jhonatan_bellaiza@hotmail.com>
2024-11-05 10:10:46 -0600

    MyFirst Commit

---
+++ src/main/java/edu/miu/asdmidtermexam/AsdMidtermExamApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.asdmidtermexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsdMidtermExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsdMidtermExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/miu/asdmidtermexam/AsdMidtermExamApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.asdmidtermexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsdMidtermExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
